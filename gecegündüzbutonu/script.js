85d87d1d98cb1bad1f95b57d03efd920a0e4ce8f
yusufardapelit <yusufardapelit34@gmail.com>
2026-07-01 20:35:41 +0300

    Add files via upload

---
+++ gecegündüzbutonu/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded',(event) => {
+	const checkbox = document.getElementById('checkbox');
+	const body = document.body;
+
+		checkbox.addEventListener('change', () => {
+				if (checkbox.checked) {
+					body.classList.remove('light-mode')	;
+					body.classList.add('dark-mode');
+				}
+				else{
+					body.classList.remove('dark-mode');
+					body.classList.add('light-mode');
+				}
+
+		});
+
+		body.classList.add('light-mode');
+
+
+}	)
